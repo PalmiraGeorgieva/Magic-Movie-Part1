228e433e037543dc819b9ecfde9e60e2a8d02b7f
Palmira Georgieva <palmira.1993@abv.bg>
2026-07-03 15:08:38 +0300

    add layout, set up about and home page

--- src/index.js
+++ src/index.js
@@ -3,17 +3,19 @@ import { engine } from 'express-handlebars';
 
 const app = express();
 
-app.engine('hbs', engine());
+app.engine('hbs', engine(
+    { extname: 'hbs'}
+));
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
 app.use(express.static('./src/static'));
 
 app.get('/', (req, res) => {
-    res.render('home', { layout: false});
+    res.render('home');
 });
 app.get('/about', (req, res) => {
-    res.render('about', { layout: false});
+    res.render('about');
 });
 
 app.listen(3000, () => console.log('Server is running on http://localhost:3000'));
