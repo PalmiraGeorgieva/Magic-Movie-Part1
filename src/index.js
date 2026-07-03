af52f93f465b9176876d110179de0319739e14ef
Palmira Georgieva <palmira.1993@abv.bg>
2026-07-03 15:32:11 +0300

    add home controller

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
+import homeController from './controllers/homeControllers.js';
 
 const app = express();
 
 app.engine('hbs', engine(
-    { extname: 'hbs'}
+    {
+        extname: 'hbs',
+        layoutsDir: './src/views/layout',
+        defaultLayout: 'main'
+    }
 ));
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
 app.use(express.static('./src/static'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-app.get('/about', (req, res) => {
-    res.render('about');
-});
+app.use('/', homeController);
 
 app.listen(3000, () => console.log('Server is running on http://localhost:3000'));
